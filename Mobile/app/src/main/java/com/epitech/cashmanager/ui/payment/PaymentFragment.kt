dfdbd43b1b17768beb993ba3bf7da2373d02208c
dylanBen <dylan.bengueddache@epitech.eu>
2019-11-04 16:27:45 +0100

    init mobile project

---
+++ Mobile/app/src/main/java/com/epitech/cashmanager/ui/payment/PaymentFragment.kt
@@ -0,0 +1,31 @@
+package com.epitech.cashmanager.ui.payment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.epitech.cashmanager.R
+
+class PaymentFragment : Fragment() {
+
+    private lateinit var paymentViewModel: PaymentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        paymentViewModel =
+            ViewModelProviders.of(this).get(PaymentViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_payment, container, false)
+        val textView: TextView = root.findViewById(R.id.text_payment)
+        paymentViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
